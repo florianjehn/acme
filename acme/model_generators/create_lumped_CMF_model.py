df754e88b2647a897c911fd7189a6ffef7eb689f
Florian Jehn <florianjehn@posteo.de>
2017-08-15 14:18:16 +0200

    Created first visualisaton method

--- acme/model_generators/create_lumped_CMF_model.py
+++ acme/model_generators/create_lumped_CMF_model.py
@@ -5,5 +5,5 @@ Created on Aug 15 13:28 2017
 
 This file is used to start the construction of a predefined CMF model. The
 user only has to provide the forcing data and the kind of techniques (
-crossover, mutation, objectivefunction).
+crossover, mutation, objective function).
 """
---
+++ acme/visualization/dna_to_image.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Aug 15 14:03 2017
+@author(s): Florian U. Jehn
+
+This tool holds functions for the visualization of the model DNA to an
+image. It takes a ACME-DNA and creates a plot of the model structure from it.
+
+It cannot be generalized as the layout of the DNA is generator/project
+specific.
+"""
+
+def visual_cmf_lumped(dna):
+    """
+
+    :param dna:
+    :return:
+    """
+    pass
