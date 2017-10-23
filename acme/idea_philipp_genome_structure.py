55de6356b2e5c5de09e2f3d2460d3f70328564ca
Florian Jehn <florianjehn@posteo.de>
2017-10-23 14:50:24 +0200

    Philipps idea on how to structure the genome

---
+++ acme/idea_philipp_genome_structure.py
@@ -0,0 +1,13 @@
+class Param:
+    def __init__(self, thres, min, max):
+        pass
+    def applies():
+        return random.random()<self.thres or test
+class Gene:
+    def __init__(self, **kwargs):
+        self.params = kwargs
+genes['snow'] = Gene(meltrate=Param(0.5,0.5,20), 
+            melt_temp=Param(0.5, -2., 2.))
+        
+        
+expressed_genes = 
