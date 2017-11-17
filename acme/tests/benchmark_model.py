fe87c335a50abff5596cea0e3264e6c30eb95c90
Florian Jehn <florianjehn@posteo.de>
2017-11-17 14:18:56 +0100

    Fixed benchmark model to working condition

--- acme/tests/benchmark_model.py
+++ acme/tests/benchmark_model.py
@@ -4,49 +4,52 @@ Created on Okt 17 14:25 2017
 @author(s): Florian U. Jehn
 
 
-Model which mimics the maximal layout of the ACME Template, to test wether this model works.
+Model which mimics the maximal layout of the ACME Template, to test whether 
+this model works.
 """
 import datetime
 import spotpy
 import numpy as np
 import cmf
-from spotpy.parameter import Uniform as param
+from spotpy.parameter import Uniform as Param
 import os
 
 
 class LumpedModelCMF:
     def __init__(self):
-        self.params = [param("tr_first_out", 0.,300),
-                       param("tr_first_river", 0., 300),
-                       param("tr_first_second", 0., 300),
-                       param("tr_second_third", 0., 300),
-                       param("tr_second_river", 0., 300),
-                       param("tr_third_river", 0., 300),
-                       param("tr_river_out", 0., 300),
-                       param("beta_first_out", 0., 4),
-                       param("beta_first_river", 0., 4),
-                       param("beta_first_second", 0., 4),
-                       param("beta_second_river", 0., 4),
-                       param("beta_second_third", 0., 4),
-                       param("beta_third_river", 0., 4),
-                       param("beta_river_out", 0., 4),
-                       param("canopy_lai", 1., 14),
-                       param("canopy_closure", 0., 1.0),
-                       param("snow_meltrate", 0.1, 15.),
-                       param("snow_melt_temp", -5.0, 5.0),
-                       param("V0_first_out", 0., 200),
-                       param("V0_first_river", 0., 200),
-                       param("V0_first_second", 0., 200),
-                       param("ETV0", 0, 200),
-                       param("fETV0", 0., 0.85)
+        self.Params = [Param("tr_first_out", 0., 300),
+                       Param("tr_first_river", 0., 300),
+                       Param("tr_first_second", 0., 300),
+                       Param("tr_second_third", 0., 300),
+                       Param("tr_second_river", 0., 300),
+                       Param("tr_third_river", 0., 300),
+                       Param("tr_river_out", 0., 300),
+                       Param("beta_first_out", 0., 4),
+                       Param("beta_first_river", 0., 4),
+                       Param("beta_first_second", 0., 4),
+                       Param("beta_second_river", 0., 4),
+                       Param("beta_second_third", 0., 4),
+                       Param("beta_third_river", 0., 4),
+                       Param("beta_river_out", 0., 4),
+                       Param("canopy_lai", 1., 14),
+                       Param("canopy_closure", 0., 1.0),
+                       Param("snow_meltrate", 0.1, 15.),
+                       Param("snow_melt_temp", -5.0, 5.0),
+                       Param("V0_first_out", 0., 200),
+                       Param("V0_first_river", 0., 200),
+                       Param("V0_first_second", 0., 200),
+                       Param("ETV0", 0, 200),
+                       Param("fETV0", 0., 0.85)
                        ]
 
-        P, T, Tmin, Tmax, Q = load_data("observed_discharge.txt", "temperature_max_min_avg.txt", "precipitation.txt", 2976.41)
-        self.Q = Q
+        prec, temp, temp_min, temp_max, q = load_data(
+            "observed_discharge.txt", "temperature_max_min_avg.txt", 
+            "precipitation.txt", 2976.41)
+        self.Q = q
         cmf.set_parallel_threads(1)
         self.project = cmf.project()
         project = self.project
-        cell = project.NewCell(0,0,0, 1000)
+        cell = project.NewCell(0, 0, 0, 1000)
 
         cell.add_storage("Snow", "S")
         cmf.Snowfall(cell.snow, cell)
@@ -57,17 +60,18 @@ class LumpedModelCMF:
 
         cmf.HargreaveET(cell.layers[0], cell.transpiration)
 
-        self.outlet = project.NewOutlet("outlet", 10,0,0)
+        self.outlet = project.NewOutlet("outlet", 10, 0, 0)
 
-        self.make_stations(P, T, Tmin, Tmax)
+        self.make_stations(prec, temp, temp_min, temp_max)
 
         self.river = cell.add_storage("River", "R")
         self.canopy = cell.add_storage("Canopy", "C")
 
-        self.begin = datetime.datetime(1979, 1,1)
-        self.end = datetime.datetime(1986,12, 31)
+        self.begin = datetime.datetime(1979, 1, 1)
+        self.end = datetime.datetime(1986, 12, 31)
 
-    def setparameters(self,tr_first_out,
+    def setparameters(self,
+                      tr_first_out,
                       tr_first_river,
                       tr_first_second,
                       tr_second_third,
@@ -91,7 +95,7 @@ class LumpedModelCMF:
                       ETV0,
                       fETV0):
         """
-        sets the parameters, all parameterized connections will be created anew
+        sets the Parameters, all Parametrized connections will be created anew
         """
         # Get all definitions from init method
         p = self.project
@@ -101,8 +105,6 @@ class LumpedModelCMF:
         third = cell.layers[2]
 
         river = self.river
-
-        canopy = self.canopy
 
         out = self.outlet
 
@@ -120,48 +122,50 @@ class LumpedModelCMF:
         cmf.kinematic_wave(second, river, tr_second_river,
                            exponent=beta_second_river)
 
-        cmf.kinematic_wave(second, third, tr_second_third, exponent=beta_second_third)
+        cmf.kinematic_wave(second, third, tr_second_third, 
+                           exponent=beta_second_third)
 
-        cmf.kinematic_wave(third, river, tr_third_river, exponent=beta_third_river)
+        cmf.kinematic_wave(third, river, tr_third_river, 
+                           exponent=beta_third_river)
 
-        cmf.kinematic_wave(river, out, tr_river_out, exponent=beta_river_out)
+        cmf.kinematic_wave(river, out, tr_river_out, 
+                           exponent=beta_river_out)
 
         # set snowmelt temperature
         cmf.Weather.set_snow_threshold(snow_melt_temp)
         # Snowmelt at the surfaces
-        snowmelt_surf = cmf.SimpleTindexSnowMelt(cell.snow,
-                                                 cell.surfacewater, cell,
-                                                 rate=snow_meltrate)
+        cmf.SimpleTindexSnowMelt(cell.snow,
+                                 cell.surfacewater, cell,
+                                 rate=snow_meltrate)
 
-        # Splits the rainfall in interzeption and throughfall
+        # Splits the rainfall in interception and throughfall
         cmf.Rainfall(cell.canopy, cell, False, True)
         cmf.Rainfall(cell.surfacewater, cell, True, False)
         # Makes a overflow for the interception storage
         cmf.RutterInterception(cell.canopy, cell.surfacewater, cell)
         # Transpiration on the plants is added
         cmf.CanopyStorageEvaporation(cell.canopy, cell.evaporation, cell)
-        # Sets the parameters for the interception
+        # Sets the Parameters for the interception
         cell.vegetation.LAI = canopy_lai
         # Defines how much throughfall there is (in %)
         cell.vegetation.CanopyClosure = canopy_closure
 
-
-    def make_stations(self, P, T, Tmin, Tmax):
+    def make_stations(self, prec, temp_avg, temp_min, temp_max):
         """
         Creates the rainfall and the climate stations
         P = time series precipitation
         T, Tmin, Tmax = time series of mean temperatur, min and max
         """
-        rainstation = self.project.rainfall_stations.add('Grebenau avg', P,
+        rainstation = self.project.rainfall_stations.add('Grebenau avg', prec,
                                                          (0, 0, 0))
         self.project.use_nearest_rainfall()
 
         # Temperature data
         meteo = self.project.meteo_stations.add_station('Grebenau avg',
                                                         (0, 0, 0))
-        meteo.T = T
-        meteo.Tmin = Tmin
-        meteo.Tmax = Tmax
+        meteo.T = temp_avg
+        meteo.Tmin = temp_min
+        meteo.Tmax = temp_max
         self.project.use_nearest_meteo()
 
         return rainstation
@@ -173,65 +177,60 @@ class LumpedModelCMF:
         """
         try:
             # Creates a solver for the differential equations
-            # solver = cmf.ImplicitEuler(self.project,1e-8)
             solver = cmf.CVodeIntegrator(self.project, 1e-8)
-            # usually the CVodeIntegrator computes the jakobi matrix only
-            # partially to save computation time. But in models with low spatial
-            # complexity this leads to a longer computational time
-            # therefore the jakob matrix is computed completely to speed things up
-            # this is done by LinearSolver = 0
             solver.LinearSolver = 0
-            c = self.project[0]
             solver.max_step = cmf.h
 
             # New time series for model results (res - result)
-            resQ = cmf.timeseries(self.begin, cmf.day)
+            sim_dis = cmf.timeseries(self.begin, cmf.day)
             # starts the solver and calculates the daily time steps
             end = self.end
-
 
             for t in solver.run(self.project.meteo_stations[0].T.begin, end,
                                 cmf.day):
                 # Fill the results
                 if t >= self.begin:
-                    resQ.add(self.outlet.waterbalance(t))
-
+                    sim_dis.add(self.outlet.waterbalance(t))
 
             # Return the filled result time series
-            return resQ
+            return sim_dis
         except RuntimeError:
             return np.array(self.Q[self.begin:self.end + datetime.timedelta(
                 days=1)]) * np.nan
 
     def simulation(self, vector):
         """
-        SpotPy expects a method simulation. This methods calls setparameters
-        and runmodels, so SpotPy is satisfied
+        SpotPy expects a method simulation. This methods calls setParameters
+        and runmodel, so SpotPy is satisfied
         """
 
-        paramdict = dict((pp.name, v) for pp, v in zip(self.params, vector))
+        paramdict = dict((pp.name, v) for pp, v in zip(self.Params, vector))
         self.setparameters(**paramdict)
-        resQ = self.runmodel()
-        return np.array(resQ)
+        sim_dis = self.runmodel()
+        return np.array(sim_dis)
 
     def evaluation(self):
         """
         For Spotpy
         """
         return np.array(
-            self.Q[self.begin:self.end + datetime.timedelta(days=1)])
+            self.Q[self.begin:self.end])
 
     def parameters(self):
         """
         For Spotpy
         """
-        return spotpy.parameter.generate(self.params)
-
-    def objectivefunction(self, simulation, evaluation):
+        return spotpy.parameter.generate(self.Params)
+    
+    @staticmethod
+    def objectivefunction(simulation, evaluation):
         """
         For Spotpy
         """
-        return spotpy.objectivefunctions.nashsutcliffe(evaluation, simulation)
+        print(len(evaluation))
+        print(len(simulation))
+        return spotpy.objectivefunctions.nashsutcliffe(evaluation,
+                                                       simulation)
 
 
 def load_data(discharge_file, temperature_file, precipitation_file,
@@ -279,11 +278,19 @@ def load_data(discharge_file, temperature_file, precipitation_file,
 
 
 if __name__ == '__main__':
-    from spotpy.algorithms import dream as Sampler
+    from spotpy.algorithms import dream as sampler
 
     parallel = 'mpi' if 'OMPI_COMM_WORLD_SIZE' in os.environ else 'seq'
 
-
     model = LumpedModelCMF()
-    sampler = Sampler(model, parallel=parallel)
-    sampler.sample(500)
+
+    # par = model.parameters()['optguess']
+    # sim = model.simulation(par)
+    # evalu = model.evaluation()
+    # like = model.objectivefunction(sim, evalu)
+    # print(like)
+    # print(evalu)
+    # print(sim)
+    sampler = sampler(model, parallel=parallel, dbname="bench.csv",
+                      dbformat="csv")
+    sampler.sample(300)
